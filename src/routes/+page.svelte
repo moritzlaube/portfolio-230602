87c1acab0df12fd32d072e5b3dab4fc8c64a2938
Moritz Laube <info@moritz-laube.de>
2023-06-02 17:14:13 +0200

    HTML and styling of home header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import me from '$lib/assets/me.png';
+</script>
+
+<div class="grid grid-cols-2">
+	<div class="wrapper col-span-2 col-start-1 row-span-full mt-28">
+		<h1
+			id="home-heading"
+			class="relative z-10 text-[155px] font-black uppercase leading-[95%] max-[1550px]:text-[10vw]"
+		>
+			Commercials<br />are dead
+		</h1>
+		<p class="relative z-10 mt-16 text-4xl uppercase">Let's create stories<br />that stick</p>
+	</div>
+	<div class="col-end-3 row-span-full flex max-h-[90vh] items-end justify-center bg-tertiary">
+		<img src={me} alt="" class="z-20 h-[80%]" />
+	</div>
+</div>
+<div>
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est quas nihil quasi id magnam libero
+		enim iste omnis expedita magni sapiente nisi totam nemo sed, ipsum fugit suscipit ab. Voluptas.
+		Libero recusandae, odio, autem distinctio porro sed temporibus consequatur vero deleniti
+		praesentium optio dolor quae molestiae officia impedit voluptate minus error doloremque quas
+		modi repudiandae? Temporibus facere voluptatibus dolorum omnis! At blanditiis nemo a voluptate
+		alias accusantium ratione in, ipsam error nostrum rem, dolorem architecto non vero autem
+		laboriosam. Nam iure quo harum, distinctio odio ad nostrum hic dolor voluptas. Necessitatibus
+		aliquid debitis aliquam omnis facere temporibus cupiditate, fugit at doloremque minus illo nihil
+		praesentium sint quidem vitae a tenetur, commodi dolore exercitationem aut inventore odio? Unde,
+		nesciunt quaerat. Ullam? Consequuntur dolor illo repellendus sequi id iste nesciunt fuga odit
+		perspiciatis, nulla facere aliquam omnis, excepturi aliquid repudiandae ipsa provident
+		praesentium distinctio at nostrum deserunt. Aliquam possimus magni delectus error! Quisquam
+		soluta facere, commodi ducimus voluptatibus mollitia? Deserunt iste, earum quos repellendus,
+		sint laudantium eius repellat soluta quasi voluptas temporibus distinctio aliquam, rem eum
+		suscipit magnam alias iure minus facilis. Esse tempore iusto, libero velit blanditiis nisi.
+		Itaque tempora ratione a ducimus nihil exercitationem molestias sed nesciunt mollitia est
+		repudiandae, voluptatem delectus enim quidem omnis magni neque, odit quis ipsum. Consequuntur
+		earum commodi quam quos ex, molestiae ab unde autem voluptates? Vel officia deserunt eum porro
+		sed vero maiores animi quos! Fuga iste nisi veniam necessitatibus ratione aliquid quaerat quam.
+		Quo, ex, voluptate aut nam repellendus perferendis, quas natus pariatur qui mollitia officia
+		optio a assumenda suscipit commodi vel! Ducimus unde sunt architecto modi minima consequatur
+		culpa vel nostrum dolorum? Labore quod tenetur, debitis ipsam nostrum architecto quae, aut hic
+		ea laboriosam quo ipsum sit. Recusandae vel assumenda accusamus excepturi veniam repellendus
+		aliquid perspiciatis quis. Accusamus quidem voluptatibus sequi ipsa?
+	</p>
+</div>
+
+<style>
+</style>
