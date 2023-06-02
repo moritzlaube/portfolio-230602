d40c30f430fcc99f963c507c8a340a74ea8eb1b1
Moritz Laube <info@moritz-laube.de>
2023-06-02 17:13:35 +0200

    Extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: ['Neue Haas', 'sans-serif']
+		},
+		colors: {
+			primary: '#1F1F1F',
+			secondary: '#C2C2C2',
+			tertiary: '#D9D9D9',
+			white: '#FFFFFF'
+		},
 		extend: {}
 	},
 	plugins: []
